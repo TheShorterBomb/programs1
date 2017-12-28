c198761393a74d02d9feed7531bf966422f0b75d
KamaalWalker <32143331+KamaalWalker@users.noreply.github.com>
2017-12-28 13:23:43 -0800

    Update GitHubKamaalTest.py

--- GitHubKamaalTest.py
+++ GitHubKamaalTest.py
@@ -1,9 +1,5 @@
-# function to remove a character from a chosen position of a string
-def remove_character(word, lint):
-    x = ''
-    # To add the letters before the removed character
-    for i in range(lint):
-        x = x + word[i]
-    # Kamaal code here:
-
-print(remove_character(input('Word?'), int(input('Letter to remove?'))))
+#Replaces a letter in a word
+word = input("Word: ")
+letter = input("Letter to remove: ")
+final = word.replace(letter,"")
+print(final)
