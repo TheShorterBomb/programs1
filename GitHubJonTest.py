01110de7db845b5df67afba4bab626177d78fdf3
KamaalWalker <32143331+KamaalWalker@users.noreply.github.com>
2017-12-28 12:11:20 -0800

    Add files via upload
    Testing the transfer and editing of code between two people

---
+++ GitHubJonTest.py
@@ -0,0 +1,19 @@
+import random
+#Generate Random Number
+nRandom = str(random.randint(1,101))
+print(nRandom)
+
+#Ask for a string
+string = input("Your String: ")
+stringlist = list(string)
+
+#Generate Another Number
+pRandom = random.randint(1,len(string))
+print(stringlist)
+
+#Join the list into one string
+stringlist.insert(pRandom, nRandom)
+string = ''.join(stringlist)
+
+#Final Output
+print(string)
